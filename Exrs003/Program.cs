5470926c442d000dcb25d80b2de9f14a87f0af92
Andrew <tropterop@yandex.ru>
2022-10-01 19:18:54 +0300

    Задание 003 выполнено

---
+++ Exrs003/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+int x = numberA % 2; 
+
+if (x == 0)
+{
+    Console.WriteLine($"Число: {numberA} четное");
+}
+
+else
+{
+    Console.WriteLine($"Число: {numberA} нечетное");
+}
