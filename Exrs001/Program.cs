333373b672b63047250714265756a71ca716a192
Andrew <tropterop@yandex.ru>
2022-10-01 18:08:06 +0300

    Задание 1 готово

---
+++ Exrs001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Add numberA:");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Add numberB:");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA > numberB)
+{
+   Console.WriteLine($"Наибольшее число из чисел: {numberA} и {numberB}, число: {numberA}");
+}
+else 
+{
+    Console.WriteLine($"Наибольшее число из чисел: {numberA} и {numberB}, число: {numberB}");
+}
