cdce959067907b5843ef7c02b955b4019d652e18
Andrew <tropterop@yandex.ru>
2022-10-01 18:38:46 +0300

    Изменение в задании 1

--- Exrs001/Program.cs
+++ Exrs001/Program.cs
@@ -1,16 +1,19 @@
-﻿Console.Clear();
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Add numberA:");
+Console.Clear();
+
+Console.Write("Введите 1 число: ");
 int numberA = int.Parse(Console.ReadLine()!);
 
-Console.Write("Add numberB:");
+Console.Write("Введите 2 число: ");
 int numberB = int.Parse(Console.ReadLine()!);
+
 
 if (numberA > numberB)
 {
-   Console.WriteLine($"Наибольшее число из чисел: {numberA} и {numberB}, число: {numberA}");
+   Console.WriteLine($"Наибольшее число: {numberA}, наименьшее число: {numberB}");
 }
 else 
 {
-    Console.WriteLine($"Наибольшее число из чисел: {numberA} и {numberB}, число: {numberB}");
+    Console.WriteLine($"Наибольшее число: {numberB}, наименьшее число: {numberA}");
 }
